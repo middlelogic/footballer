0aded8a98901174698cc73cee2b680091157633f
Tim Cooper <timothy.cooper@shea.ca>
2015-10-18 17:36:13 -0400

    revert changes

--- client/home/content/content.js
+++ client/home/content/content.js
@@ -1,21 +1,25 @@
-// var countdown = new ReactiveCountdown(30);
-//
-// countdown.start(function() {
-//
-//     // do something when this is completed
-//     countdown.start();
-//
-//     $('.menu .item').tab();
-//
-//     Meteor.call('getGameData',function(response) {
-//       // Session.set('noRender', true);
-//       // Meteor.setTimeout(function() {
-//         // Session.set('noRender', false);
-//       // }, 1);
-//     });
-// });
+var countdown = new ReactiveCountdown(30);
+
+countdown.start(function() {
+
+    // do something when this is completed
+    countdown.start();
+
+    $('.menu .item').tab();
+
+    Meteor.call('getGameData',function(response) {
+      // Session.set('noRender', true);
+      // Meteor.setTimeout(function() {
+        // Session.set('noRender', false);
+      // }, 1);
+    });
+});
 
 Template.mainContent.helpers({
+  getCountdown: function() {
+        var number = countdown.get();
+        return number >= 10 ? '0:' + countdown.get() : '0:0' + countdown.get();
+  },
   currentUser: function(){
     return Meteor.user();
   }
@@ -34,10 +38,6 @@ Template.mainContent.rendered = function() {
     $.getJSON( "http://www.nfl.com/liveupdate/scorestrip/ss.json", {} )
       .done(function( json ) {
         Session.set('week', json.w);
-        Meteor.call('getGameData',function(response) {
-          Router.current().render(Template.mainContentScores, { to: 'mainContentScores' });
-          Router.current().render(Template.mainContentVersus, { to: 'mainContentVersus' });
-        });
       })
       .fail(function( jqxhr, textStatus, error ) {
         var err = textStatus + ", " + error;
